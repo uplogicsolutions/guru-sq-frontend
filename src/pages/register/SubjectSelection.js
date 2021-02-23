25c6225e53e7849ca541d9ae543caa9777b5c028
samapara <mapara.shubham@gmail.com>
2021-02-23 09:47:52 +0530

    Added Subject Selection Page

--- src/pages/register/SubjectSelection.js
+++ src/pages/register/SubjectSelection.js
@@ -1,29 +1,88 @@
 import React, { useState } from 'react';
-import { TagPicker } from 'rsuite';
+import { Panel, Row, Col, Input, Form, DatePicker, InputPicker, Button, IconButton, Icon, TagPicker } from 'rsuite';
 
 const SubjectSelection = props => {
 
     const [core_subjects, setCoreSubjects] = useState([])
+    const [improvement_subjects, setImprovementSubjects] = useState([])
+    const [professional_guidance_subjects, setProfessionalGuidanceSubjects] = useState([])
 
-    const test_data =[
+    const subjects =[
         {
-            "label": "Private",
-            "value": "private",
+            "label": "Maths",
+            "value": "0",
             "role": ""
         },
         {
-            "label": "Goverment",
-            "value": "goverment",
+            "label": "Science",
+            "value": "1",
+            "role": ""
+        },
+        {
+            "label": "English",
+            "value": "2",
             "role": ""
         }
     ]
+
+    const handleSubmit = () => {
+        const data = {
+            core_subjects,
+            improvement_subjects,
+            professional_guidance_subjects
+        }
+
+        console.log(data);
+    }
     return (
-        <>
-            <TagPicker
-                data={test_data}
-                onSelect={(val) => { setCoreSubjects(val)}}
-            />
-        </>
+        <Panel style={{ background: 'white' }} shaded>
+            <Form>
+                <Row>
+                    <Col xs={24} sm={24} md={24}>
+                        <h5>Which subjects are your core ?</h5>
+                    </Col>
+                    <Col className="mt-2" xs={24} sm={24} md={24}>
+                    <TagPicker
+                        block
+                        data={subjects}
+                        onSelect={(val) => { setCoreSubjects(val)}}
+                        placeholder="Select Core Subjects"
+                    />
+                    </Col>     
+                </Row>
+                <Row>
+                    <Col xs={24} sm={24} md={24}>
+                        <h5>Subjects you can improve ?</h5>
+                    </Col>
+                    <Col className="mt-2" xs={24} sm={24} md={24}>
+                    <TagPicker
+                        block
+                        data={subjects}
+                        onSelect={(val) => { setImprovementSubjects(val)}}
+                        placeholder="Select Imrpovement Subjects"
+                    />
+                    </Col>     
+                </Row>
+                <Row>
+                    <Col xs={24} sm={24} md={24}>
+                        <h5>Subjects you prefer professional guidance ?</h5>
+                    </Col>
+                    <Col className="mt-2" xs={24} sm={24} md={24}>
+                    <TagPicker
+                        block
+                        data={subjects}
+                        onSelect={(val) => { setProfessionalGuidanceSubjects(val)}}
+                        placeholder="Select Guidance Subjects"
+                    />
+                    </Col>     
+                </Row>
+                <Row>
+                    <Col xs={24} sm={24} md={24}>
+                        <Button onClick={handleSubmit} block appearance="primary"> <b>CONTINUE</b> </Button>
+                    </Col>
+                </Row>
+            </Form>
+        </Panel>
     )
 }
 
