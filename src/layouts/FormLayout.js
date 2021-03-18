132dc6c1a44cc0fd61134e81b800ee402344e611
shubhangik1511 <shubhangik1511@gmail.com>
2021-03-18 20:36:30 +0530

    Some UI changes in form layout.

--- src/layouts/FormLayout.js
+++ src/layouts/FormLayout.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import {FlexboxGrid, Col, Grid} from 'rsuite';
+import { FlexboxGrid, Col, Grid, Row } from 'rsuite';
+import Logo from 'assets/gurusq.png';
 
 const FormLayout = ({ children }) => (
-    <FlexboxGrid justify="center" align="middle" style={{background:'lightgray', minHeight:'100vh'}}>
-        <FlexboxGrid.Item componentClass={Col} colspan={20} sm={20} xs={20} md={8}>
-            {children}
-        </FlexboxGrid.Item>
-    </FlexboxGrid>
+    <div style={{ width: '100%', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', paddingTop: '10px' }}>
+        <img className="p-5" src={Logo} width="250px" />
+        <FlexboxGrid justify="center" align="middle" style={{ background: 'white', width: '100%', flexGrow: 1 }}>
+            <FlexboxGrid.Item componentClass={Col} colspan={20} sm={20} xs={20} md={8}>
+                {children}
+            </FlexboxGrid.Item>
+        </FlexboxGrid>
+    </div>
 );
 
 const FormLayoutRoute = ({ component: Component, ...rest }) => {
