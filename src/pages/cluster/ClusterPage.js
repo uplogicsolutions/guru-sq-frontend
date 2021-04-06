8335793eba7511b67fa08c256d532d31bf2d166a
samapara <mapara.shubham@gmail.com>
2021-04-06 09:01:18 +0530

    Fixed Bugs #IMP3 #IMP4 #L01

--- src/pages/cluster/ClusterPage.js
+++ src/pages/cluster/ClusterPage.js
@@ -32,13 +32,11 @@ const Tabs = () => {
         }));
         dispatch(getClusterMessages());
         setInputMessage('')
-        scrollToRef.current?.scrollIntoView({ behavior: "smooth" })
 
     }
 
     socket.on('cluster_messages', (data) => {
         dispatch(handleClusterMessageEvent(data));
-        scrollToRef.current?.scrollIntoView({ behavior: "smooth" })
     })
 
     return (
@@ -77,7 +75,7 @@ const Tabs = () => {
                                         activeClassName="opacity-100 duration-500 translate-x-0"
                                         inactiveClassName="opacity-0 hidden -translate-x-2">
 
-                                        <div className="w-full h-full overflow-y-auto mb-10 flex-col" style={{ flexGrow: 1, flexShrink: 1, flexBasis: 0 }}>
+                                        <div className="w-full h-full overflow-y-auto mb-10 flex flex-col" style={{ flexGrow: 1, flexShrink: 1, flexBasis: 0 }}>
 
                                             {messages.map(message => (
                                                 <div className={`flex px-4 my-2 w-max ${message.sender_id === user_id ? 'self-end' : 'self-start'} items-center bg-green-100 flex-row`}>
--- src/pages/landing/LandingPage.js
+++ src/pages/landing/LandingPage.js
@@ -4,19 +4,38 @@ import { useSelector, useDispatch } from 'react-redux';
 import Logo from 'assets/gurusq.png';
 import Sqaure from 'assets/gradient50.png';
 import Landing from 'assets/landing.mp4'
-import { Button, Col, Input, Row, Alert, Loader } from 'rsuite';
+import { Button, Col, Input, Row, Alert, Loader, FormGroup, Form } from 'rsuite';
+//Form Validation
+import { useForm, Controller } from "react-hook-form";
+
+//Custom Components
+import Danger from 'components/alerts/Danger';
 import { setLoading, setRedirect, setRedirectUrl, setLoggedIn } from 'auth/store';
 import { register } from 'api/auth';
 
 const LandingPage = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    // const [username, setUsername] = useState('');
+    // const [password, setPassword] = useState('');
     const { loading } = useSelector((state) => state.auth);
     const history = useHistory();
     const dispatch = useDispatch();
 
-    const handleRegister = async () => {
+    const { control, errors, handleSubmit } = useForm();
+
+    const isValidUsername = (username) => {
+        const email_regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const mobile_regex = /^([6-9][0-9]{9})$/
+        if (email_regex.test(username))
+            return true
+        else if (mobile_regex.test(username))
+            return true
+        return false
+    }
+    const handleRegister = async (data) => {
         dispatch(setLoading(true));
+        const { username, password } = data;
+        console.log(username, password);
+        return;
         let response = await register({ username, password });
         dispatch(setLoading(false))
         if (response.type == 'success') {
@@ -41,21 +60,43 @@ const LandingPage = () => {
                 <img src={Sqaure} width={500} height={500} style={{ position: 'absolute', top: -80, right: -90, rotate: '65deg' }} />
 
                 <div style={{ position: 'relative' }} className="flex flex-col">
-                    <Row className="p-3 mt-3">
-                        <Col className="mt-2 md:mt-0" xs={24} md={8}>
-                            <Input name="username" className="" value={username} onChange={(val) => setUsername(val)} placeholder="Username" />
-                        </Col>
-                        <Col className="mt-2 md:mt-0" xs={24} md={8}>
-                            <Input name="password" className="" value={password} onChange={(val) => setPassword(val)} placeholder="Password" type="password" />
-                        </Col>
-                        <Col className="mt-2 md:mt-0" xs={24} md={8}>
-                            <div className="flex flex-col">
-                                <Button appearance="primary" className="" onClick={handleRegister}>Register</Button>
-                                <a className="mt-1 self-center" style={{ cursor: "pointer" }} onClick={() => history.push('/login')}>Already registered? Login</a>
-                            </div>
-                        </Col>
-
-                    </Row>
+                    <Form onSubmit={handleSubmit(handleRegister)}>
+                        <Row className="p-3 mt-3">
+                            <Col className="mt-2 md:mt-0" xs={24} md={8}>
+                                <FormGroup>
+                                    <Controller
+                                        name="username"
+                                        control={control}
+                                        defaultValue=""
+                                        rules={{
+                                            required: true,
+                                            validate: value => isValidUsername(value)
+                                        }}
+                                        render={({ onChange, value }) =>
+                                            <Input
+                                                name="username"
+                                                onChange={(text, e) => onChange(e)}
+                                                value={value}
+                                                placeholder="Mobile or Email"
+                                            />
+                                        }
+                                    />
+                                    
+                                    {errors.username?.type === 'required' && <Danger>*This field is required</Danger>}
+                                    {errors.username?.type === 'validate' && <Danger>*Please enter valid mobile or email</Danger>}
+                                </FormGroup>
+                            </Col>
+                            <Col className="mt-2 md:mt-0" xs={24} md={8}>
+                                {/* <Input name="password" className="" value={password} onChange={(val) => setPassword(val)} placeholder="Password" type="password" /> */}
+                            </Col>
+                            <Col className="mt-2 md:mt-0" xs={24} md={8}>
+                                <div className="flex flex-col">
+                                    <Button appearance="primary" type="submit" className="">Register</Button>
+                                    <a className="mt-1 self-center" style={{ cursor: "pointer" }} onClick={() => history.push('/login')}>Already registered? Login</a>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Form>
                     <hr />
                     <div className="p-3 flex justify-center w-full" >
                         <video autoPlay loop controls muted className="w-full" style={{ height: 'auto' }} >
